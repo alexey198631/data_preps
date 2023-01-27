182c920c2353623d855132c021e505c0ad7418cf
Alex <al.an.gukov@gmail.com>
2023-01-27 23:20:20 +0100

    prj 23003 correction of vev.py

--- prj_23003_clean_customer_list/lev.py
+++ prj_23003_clean_customer_list/lev.py
@@ -2,30 +2,33 @@ import pandas as pd
 import jellyfish
 
 # Read the Excel file
-df = pd.read_excel("data_files/initial_companies.xlsx")
+df = pd.read_excel("data_files/the_first_iteration.xlsx")
 
 # Extract the column of company names
-company_names = df["Customer Name"].tolist()
+company_names = df["test"].tolist()
 final_result = pd.DataFrame()
 
 # Set a threshold for similarity score
-threshold = 0.5
+threshold = 0.90
 border = len(company_names)
+counter = 0
 
 # Compare each pair of company names
-for i in range(1000):
-    for j in range(i+1, 1000):
+for i in range(border):
+    for j in range(i+1, border):
         # Calculate Jaro-Winkler distance
         distance = jellyfish.jaro_winkler(company_names[i], company_names[j])
 
         # Check if the distance is greater than the threshold
-        if distance > threshold:
-            final_result.loc[i, ['company']] = company_names[i]
-            final_result.loc[i, ['similar']] = company_names[j]
-            final_result.loc[i, ['distance']] = round(distance, 2)
+        if threshold < distance < 1:
+            final_result.loc[counter, ['company']] = company_names[i]
+            final_result.loc[counter, ['similar']] = company_names[j]
+            final_result.loc[counter, ['distance']] = round(distance, 2)
+            counter += 1
+            print(counter, i, j, company_names[i], company_names[j], round(distance, 2))
 
 final_result = final_result.sort_values(by=['distance'], ascending=False)
 
 writer = pd.ExcelWriter('data_files/similar.xlsx', engine='xlsxwriter')
-final_result.to_excel(writer, sheet_name='good')
+final_result.to_excel(writer, sheet_name='similarity_check')
 writer.save()
